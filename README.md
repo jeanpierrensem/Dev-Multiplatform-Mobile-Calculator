1a8899e03675ed8d72f7dfa54c6e6e6ccefe2e7a
jeanpierrensem2019 <50254761+jeanpierrensem2019@users.noreply.github.com>
2026-06-17 14:57:48 +0200

    adding comments

--- README.md
+++ README.md
@@ -17,6 +17,13 @@ Ce TP consiste à modifier le projet de base android (Empty view Activity) pour 
 
 ## Prerequis communs
 
+• L’objectif étant de créer une simple application mobile Android qui permet de :
+• Saisir un nombre représentant un montant en Euro dans un composant de type
+« EditText »
+• Convertir le montant saisi en DH en le multipliant par le taux de change
+• Afficher le résultat dans un champ de type TextView.
+• Ajouter l’historiques des conversions dans un ListView
+
 - Installer Android Studio (V)
 
 <h1>  </h1> 
--- app/src/main/java/jp/dev/calculator/MainActivity.java
+++ app/src/main/java/jp/dev/calculator/MainActivity.java
@@ -6,13 +6,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,33 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         // Code à ajouter
         EditText editTextAmount=findViewById(R.id.editTextAmount);
         Button btnCompute=findViewById(R.id.btnCompute);
         TextView textViewResult=findViewById(R.id.textViewResult);
         ListView listViewResult=findViewById(R.id.listViewResults);
+
         List<String> data=new ArrayList<>();
+
+        //Création de l'adapter et association à la listview pour affichage
         ArrayAdapter<String> stringArrayAdapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listViewResult.setAdapter(stringArrayAdapter);
+
+        //Gestionnaire de l'évènement Onclick et logique metier
         btnCompute.setOnClickListener((view)->{
             double amount=Double.parseDouble(editTextAmount.getText().toString());
             double result=amount*11;
             textViewResult.setText(String.valueOf(result));
             data.add(amount+"=>"+result);
+            //on notifie le subscribe du changement des data
+            //et la listview fait le refresh
             stringArrayAdapter.notifyDataSetChanged();
             editTextAmount.setText("");
-
-
         });
-
-
-
-
-
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
     }
 }
